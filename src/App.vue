57355f87fc29ebe37185b7930cc0f8d75add8265
Nina Matti <ninadesireematti@gmail.com>
2021-02-08 10:54:09 +0100

    Adjusted and removed boilerplate code and added basic todo data in App component

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,50 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>Helloho</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: "Todo 1",
+          completed: false
+        },
+        {
+          id: 2,
+          title: "Todo 2",
+          completed: false
+        },
+        {
+          id: 3,
+          title: "Todo 3",
+          completed: false
+        },
+        
+      ]
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  line-height: 1.4;
 }
 </style>
