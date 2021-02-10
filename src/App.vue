ec8c97add6ffa093dbbe50a5e90cd87bdc57d155
Nina Matti <ninadesireematti@gmail.com>
2021-02-10 21:54:06 +0100

    Added new components to App.vue, bound methods

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,25 @@
 <template>
-  <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" v-on:complete-todo="changeStatus"/>
+  <div>
+    <Header />
+    <AddTodo v-on:add-todo="addTodo"/>
+    <Todos 
+      v-bind:todos="todos" 
+      v-on:del-todo="deleteTodo" 
+      v-on:complete-todo="changeStatus"/>
+  </div>
 </template>
 
 <script>
-import Todos from './components/Todos'
+import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
+import Header from './components/layout/Header';
 
 export default {
   name: 'App',
   components: {
-    Todos
+    Todos,
+    Header,
+    AddTodo
   },
   data() {
     return {
@@ -38,6 +49,9 @@ export default {
     },
     changeStatus(id) {
       this.todos.forEach(todo => todo.id === id ? todo.completed = !todo.completed : '')
+    },
+    addTodo(newTodo) {
+      this.todos = [...this.todos, newTodo]
     }
   }
 }
@@ -54,5 +68,18 @@ export default {
 body {
   font-family: Arial, Helvetica, sans-serif;
   line-height: 1.4;
+}
+
+.btn {
+  display: inline-block;
+  border: none;
+  background: #555;
+  color: #fff;
+  padding: 7px 20px;
+  cursor: pointer;
+}
+
+.btn:hover {
+  background: #666;
 }
 </style>
