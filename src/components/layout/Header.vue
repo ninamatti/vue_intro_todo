b6e2263a72fa81ab47f2e66c9fbef361b372bfdf
Nina Matti <ninadesireematti@gmail.com>
2021-02-10 21:53:12 +0100

    Added Header component

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -10,4 +10,18 @@ export default {
 }
 </script>
 
+<style scoped>
+    .header {
+        background: #333;
+        color: #fff;
+        text-align: center;
+        padding: 10px;
+    }
+
+    .header a {
+        color: #fff;
+        padding-right: 5px;
+    }
+</style>
+
 
